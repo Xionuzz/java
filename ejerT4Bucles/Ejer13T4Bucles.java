0addd1419e1cafcc94282da234ec68f00760ec27
Izan <izanfarmonde@hotmail.es>
2019-10-22 19:47:41 +0200

    Modificado un código

--- ejerT4Bucles/Ejer13T4Bucles.java
+++ ejerT4Bucles/Ejer13T4Bucles.java
@@ -3,15 +3,15 @@ import java.util.Random;
 public class Ejer13T4Bucles {
 
     public static void main(String Args[]) {
-
-	int moneda = 0;
-	int cara = 0;
+    
+    int moneda = 0;
+    int cara = 0;
     int cruz = 0;
     float resultadoCara = 0f;
     float resultadoCruz = 0f;
 
-	
-	Random rnd = new Random();
+
+    Random rnd = new Random();
     
     /* Lanza la moneda un millón de veces, añade un ounto a un contador cada vez
     /* que sale cara o cruz. */
@@ -33,9 +33,9 @@ public class Ejer13T4Bucles {
     resultadoCruz = cruz;
 
     // Mostrar resultado por pantalla.
-
-	System.out.println("/// RESULTADOS ///");
-	System.out.println("Total monedas lanzadas: " + (cara+cruz));
+    
+    System.out.println("/// RESULTADOS ///");
+    System.out.println("Total monedas lanzadas: " + (cara+cruz));
 	System.out.println("Total caras: " + cara);
 	System.out.println("Total cruces: " + cruz);
     System.out.println("Porcentaje de caras: " + (resultadoCara / 1000000)  * 100 + "%");
