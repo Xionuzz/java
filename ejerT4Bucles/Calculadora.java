c176ce76f489650937620e55127bb4b0192a1f63
Izan <izanfarmonde@hotmail.es>
2019-10-24 22:39:51 +0200

    Actualizado ejercicio de la calculadora

--- ejerT4Bucles/Calculadora.java
+++ ejerT4Bucles/Calculadora.java
@@ -13,49 +13,66 @@ public class Calculadora {
         boolean bucleCalculadora = true;
 
 
-        System.out.println("---CALCULADORA---");
-        System.out.println("Elige una opción: ");
-        System.out.println("1. Sumar");
-        System.out.println("2. Restar");
-        System.out.println("3. Multiplicar");
-        System.out.println("4. Dividir");
-        System.out.println("5. Resto división");
-        System.out.println("------------------");
-        System.out.println("0. Salir");
-        System.out.println();
+        while(bucleCalculadora){
 
-        System.out.println("Introduzca el número correspondiente a la operación: ");
-        numeroIntroducido = scan.nextInt();
+            System.out.println();
+            System.out.println("---CALCULADORA---");
+            System.out.println("Elige una opción: ");
+            System.out.println("1. Sumar");
+            System.out.println("2. Restar");
+            System.out.println("3. Multiplicar");
+            System.out.println("4. Dividir");
+            System.out.println("5. Resto división");
+            System.out.println("------------------");
+            System.out.println("0. Salir");
+            System.out.println();
+    
+            System.out.print("Introduzca el número correspondiente a la operación que quiere llevar a cabo: ");
+            numeroIntroducido = scan.nextInt();
 
-        switch(numeroIntroducido){
+            switch(numeroIntroducido){
 
-            case 1:
-                System.out.print("Introduzca el primer número: ");
-                primerNumero = scan.nextInt();
-                System.out.println("Introduzca el segundo número: ");
-                segundoNumero = scan.nextInt();
-                System.out.println("El resultado de la suma es: " + (resultado = primerNumero + segundoNumero));
+                case 1:
+                    System.out.print("Introduzca el primer número: ");
+                    primerNumero = scan.nextInt();
+                    System.out.print("Introduzca el segundo número: ");
+                    segundoNumero = scan.nextInt();
+                    System.out.println("El resultado de la suma es: " + (resultado = primerNumero + segundoNumero));
                 break;
- 
-            case 2:
-                System.out.println("Introduzca el primer número: ");
-                primerNumero = scan.nextInt();
-                System.out.println("Introduzca el segundo número: ");
+                case 2:
+                    System.out.print("Introduzca el primer número: ");
+                    primerNumero = scan.nextInt();
+                    System.out.print("Introduzca el segundo número: ");
+                    segundoNumero = scan.nextInt();
+                    System.out.println("El resultado de la resta es: " + (resultado = primerNumero - segundoNumero));
                 break;
-            case 3:
-                ;
+                case 3:
+                    System.out.print("Introduzca el primer número: ");
+                    primerNumero = scan.nextInt();
+                    System.out.print("Introduzca el segundo número: ");
+                    segundoNumero = scan.nextInt();
+                    System.out.println("El resultado de la multiplicación es: " + (resultado = primerNumero * segundoNumero));
                 break;
-            case 4:
-                ;
+                case 4:
+                    System.out.print("Introduzca el primer número: ");
+                    primerNumero = scan.nextInt();
+                    System.out.print("Introduzca el segundo número: ");
+                    segundoNumero = scan.nextInt();
+                    System.out.println("El resultado de la división es: " + (resultado = primerNumero / segundoNumero));
                 break;
-            case 5:
-                ;
+                case 5:
+                    System.out.print("Introduzca el primer número: ");
+                    primerNumero = scan.nextInt();
+                    System.out.print("Introduzca el segundo número: ");
+                    segundoNumero = scan.nextInt();
+                    System.out.println("El resultado del resto de la división es: " + (resultado = primerNumero % segundoNumero));
                 break;
-            case 0:
-                ;
+                case 0:
+                    System.out.println("¡Gracias por usar la calculadora!");
+                    bucleCalculadora = false;
                 break;
-        
+            
+            }
         }
-
     }
 }
