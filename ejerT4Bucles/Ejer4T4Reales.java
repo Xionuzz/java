a3367bdcc6a519567d248b258fb689bab6d6da20
Izan <izanfarmonde@hotmail.es>
2019-10-29 20:18:09 +0100

    Añadidos nuevos ejercicios

---
+++ ejerT4Bucles/Ejer4T4Reales.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ejer4T4Reales{
+
+    public static void main(String[] args) {
+        
+        String s = "";
+        
+        for(int i = 1; i <= 9; i++){
+
+            s = s + i;
+            System.out.printf("%1s\n", s);
+        }
+    }
+}
---
+++ ejerT4Bucles/Ejer5T4Reales.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ejer5T4Reales{
+
+    public static void main(String[] args) {
+
+        String s = "";
+        
+        for (int i = 1; i <= 9; i++)
+
+            s = s + i;
+            System.out.printf("\u001B[1;38;5;9m%9s\n\u001B[0m");
+    }
+}
