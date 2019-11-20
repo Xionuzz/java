ba90c670acc48190e97049b167183cb71a4128a1
Izan <izanfarmonde@hotmail.es>
2019-11-20 17:38:58 +0100

    Añadidos nuevos ejercicios

---
+++ ejerT6Métodos/Ejer20T6M.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ejer20T6M {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner (System.in);
+
+        int fechaNacimiento = 0;
+
+        System.out.print("Introduce tu fecha de nacimiento (dd/mm/aa): ");
+        fechaNacimiento = scan.nextInt();
+
+        
+    }
+}
